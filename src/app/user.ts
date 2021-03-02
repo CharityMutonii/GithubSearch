51e047399e83954ff3aa42daff65eed8c836a2e6
Charity Mutoni <charitytoni0@gmail.com>
2021-03-02 07:17:39 +0200

    user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,14 @@
 export class User {
+ constructor(
+        public login:string,
+        public avatar_url:string,
+        public email:string,
+        public location:string,
+        public memberSince:Number,
+        public repo:number,
+        public followers:number,
+        public following:number,
+        public html_url:string
+      ){}
 }
+
