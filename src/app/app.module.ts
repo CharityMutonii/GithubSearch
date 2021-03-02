f5a4c3ba812a05e051be998844b560faec992f9b
Charity Mutoni <charitytoni0@gmail.com>
2021-03-02 07:29:41 +0200

    created date-count pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
