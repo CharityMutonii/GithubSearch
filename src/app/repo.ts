c5b2a17f4eb09c900946c57f2ef6a3538d2c181a
Charity Mutoni <charitytoni0@gmail.com>
2021-03-02 07:19:37 +0200

    repo class

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,2 +1,11 @@
 export class Repo {
+    constructor(
+        public repo:number,
+        public description:string,
+        public name:string,
+        public forks:number,
+        public language:string,
+        public html_url:string,
+        public date_created
+      ){}
 }
